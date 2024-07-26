ff831391a12dac1709c9bac22dab36e9c6f8fefb
dpftlel21 <dkdnmju@naver.com>
2024-07-26 15:39:03 +0900

    feat: 회원가입 이메일인증 구현 (ing)

--- foori/src/components/signup/SignUpBox.tsx
+++ foori/src/components/signup/SignUpBox.tsx
@@ -1,67 +1,77 @@
+import { useState } from "react";
 import Logo from "../common/Logo";
 import Email from "../../assets/images/email.png";
 import Lock from "../../assets/images/lock.png";
 import Person from "../../assets/images/person.png";
 import Calendar from "../../assets/images/calendar.png";
+import { postData } from "../../util/api";
+
 
 const SignUpBox = () => {
-  const SignUpBox =
-    "w-[35%] h-[60%] flex flex-col justify-center items-center bg-gray-100 bg-opacity-40 border border-gray-400 rounded-md shadow-md text-sm";
-  const InputContainer =
-    "w-full flex flex-col justify-center items-center my-[1%]";
-  const InputStyle = "w-[45%] h-[3vh] rounded-md";
-  const InputTitle = "w-[45%] flex justify-start items-center mb-[1%]";
-  const ButtonStyle =
-    "w-[25%] h-[5vh] mt-[4%] bg-[#FF800B] text-white rounded-md hover:bg-[#fcb69f] transition duration-500 ease-in-out";
+  const SignUpBox = "w-[35%] h-[60%] flex flex-col justify-center items-center bg-gray-100 bg-opacity-40 border border-gray-400 rounded-md shadow-md text-sm";
+  const InputContainer = "w-full flex flex-col justify-center items-center my-[1%]";
+  const InputStyle  = "w-[45%] h-[5vh] p-[1%] rounded-md ";
+  const InputTitle  = "w-[45%] flex justify-start items-center mb-[1%]";
+  const ButtonStyle = "w-[25%] h-[5vh] mt-[4%] bg-[#FF800B] text-white rounded-md hover:bg-[#fcb69f] transition duration-500 ease-in-out";
 
-  // 상수 데이터
-  // 년
-  const YEAR = [];
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    nickname: "",
+    birth: "",
+  });
 
-  const nowYear = new Date().getFullYear();
-  for (let i = 1960; i <= nowYear; i++) {
-    YEAR.push(i);
-  }
+  // INPUT 값 변경
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-  // 월
-  const MONTH = [];
 
-  for (let i = 1; i <= 12; i++) {
-    let m = String(i).padStart(2, "0");
-    MONTH.push(m);
-  }
-
-  // 일
-  const DAY = [];
-  for (let i = 1; i <= 31; i++) {
-    let d = String(i).padStart(2, "0");
-    DAY.push(d);
+  const handleCertify = async () => {
+     try{
+        const result = await postData(`/verify/email`, { email: formData.email });
+        console.log("result", result);
+     }
+     catch(error){
+        console.error('Error fetching data:', error);
+     }
   }
 
   return (
     <>
       <Logo />
-      <div className={SignUpBox}>
+      <form className={SignUpBox}>
         {/* 아이디 */}
         <div className={InputContainer}>
           <div className={InputTitle}>
-            <img src={Email} alt="" className="w-10" />
+            <img src={Email} alt="" className="w-[12%]" />
             <label htmlFor="username" className="ml-[1%]">
-              Email
+              이메일
             </label>
+            {/* 인증하기 버튼 */}
+            <button 
+            className="w-[25%] h-[3vh] ml-[1%] bg-[#FF800B] text-white text-xs  rounded-md hover:bg-[#fcb69f] transition duration-500 ease-in-out"
+            onClick={handleCertify}
+            >
+              인증하기
+            </button>
           </div>
           <input
             className={InputStyle}
-            type="text"
+            type="email"
+            name="email"
+            value={formData.email}
             placeholder="이메일을 입력하세요."
+            onChange={handleChange}
           />
         </div>
         {/* 비밀번호 */}
         <div className={InputContainer}>
           <div className={InputTitle}>
-            <img src={Lock} alt="" className="w-10" />
+            <img src={Lock} alt="" className="w-[12%]" />
             <label htmlFor="username" className="ml-[1%]">
-              Password
+              비밀번호
             </label>
           </div>
           <input
@@ -73,9 +83,9 @@ const SignUpBox = () => {
         {/* 닉네임 */}
         <div className={InputContainer}>
           <div className={InputTitle}>
-            <img src={Person} alt="" className="w-10" />
+            <img src={Person} alt="" className="w-[12%]" />
             <label htmlFor="username" className="ml-[1%]">
-              NickName
+              닉네임
             </label>
           </div>
           <input
@@ -87,32 +97,16 @@ const SignUpBox = () => {
         {/* 생년월일 */}
         <div className={InputContainer}>
           <div className={InputTitle}>
-            <img src={Calendar} alt="사람 모양" className="w-10" />
+            <img src={Calendar} alt="사람 모양" className="w-[12%]" />
             <label htmlFor="birth" className="ml-[1%]">
-              Your Birth
+              생년월일
             </label>
           </div>
-          <div className="w-[43%] flex justify-start">
-            <select name="year" id="">
-              {YEAR.map((y) => {
-                return <option key={y}>{y}</option>;
-              })}
-            </select>
-            <select className="mx-[4%]" name="month" id="">
-              {MONTH.map((m) => {
-                return <option key={m}>{m}</option>;
-              })}
-            </select>
-            <select name="day" id="">
-              {DAY.map((d) => {
-                return <option key={d}>{d}</option>;
-              })}
-            </select>
-          </div>
+            <input className={InputStyle} type="date" />
           {/* 로그인 버튼 */}
-          <button className={ButtonStyle}>완료하기</button>
+          <button type="submit" className={ButtonStyle}>완료하기</button>
         </div>
-      </div>
+      </form>
     </>
   );
 };
