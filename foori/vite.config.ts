02f9b43cca943c1388c05f9a22ca7b4ca88492fb
dpftlel21 <dkdnmju@naver.com>
2025-01-20 23:04:44 +0900

    배포 돼라

--- foori/vite.config.ts
+++ foori/vite.config.ts
@@ -2,30 +2,20 @@ import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
+  //환경 변수
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
-    build: {
-      target: 'esnext',
-      minify: 'esbuild',
-      // 청크 크기 최적화
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom'],
-            // 큰 라이브러리들 분리
-          },
-        },
-      },
-      // 빌드 캐시 활성화
-      cache: true,
-    },
     server: {
       proxy: {
         '/api': {
           target: env.VITE_BACK_URL,
           changeOrigin: true,
+          //secure: false,
+          //credentials: "include",
+          //ws: true, // 실시간 채팅, 알림 데이터 업데이트 등,,,
+          //rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
