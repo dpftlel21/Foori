342b0e5353e6f86ca9782e97697d3026f175d86f
dpftlel21 <dkdnmju@naver.com>
2025-01-20 22:49:35 +0900

    bug: 배포 시간 timeout

--- foori/vite.config.ts
+++ foori/vite.config.ts
@@ -1,27 +1,35 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
-import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-
-  //환경 변수
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
+    build: {
+      target: 'esnext',
+      minify: 'esbuild',
+      // 청크 크기 최적화
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+          },
+        },
+      },
+      // 빌드 캐시 활성화
+      cache: true,
+    },
     server: {
       proxy: {
         '/api': {
           target: env.VITE_BACK_URL,
           changeOrigin: true,
-          //secure: false,
-          //credentials: "include",
-          //ws: true, // 실시간 채팅, 알림 데이터 업데이트 등,,,
-          //rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
     },
     css: {
-    postcss: './postcss.config.js',
-  },
+      postcss: './postcss.config.js',
+    },
   };
 });
