3b7c6d900d38173e53b94a070a703028cb0f144f
dpftlel21 <dkdnmju@naver.com>
2025-01-22 20:00:47 +0900

    fix: scripts 수정

--- foori/scripts/before_install.sh
+++ foori/scripts/before_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# 기존 파일 정리
-if [ -d /home/ec2-user/Foori/foori ]; then
-    rm -rf /home/ec2-user/Foori/foori/*
-fi
-
-# 디렉토리 생성
-mkdir -p /home/ec2-user/Foori/foori
+# 기존 배포 디렉토리 정리
+rm -rf /home/ec2-user/Foori/foori/dist
+mkdir -p /home/ec2-user/Foori/foori/dist
--- foori/scripts/start_application.sh
+++ foori/scripts/start_application.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd /home/ec2-user/Foori/foori
+# 권한 설정
+sudo chown -R ec2-user:ec2-user /home/ec2-user/Foori
 
 # nginx 설정 복사 및 재시작
-sudo cp nginx.conf /etc/nginx/conf.d/default.conf
+sudo cp /home/ec2-user/Foori/foori/nginx.conf /etc/nginx/conf.d/default.conf
 sudo systemctl restart nginx
