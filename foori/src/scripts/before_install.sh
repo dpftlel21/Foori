8bcc8cfafa66d81a52d022a6c3d320381597836a
dpftlel21 <dkdnmju@naver.com>
2025-01-22 18:59:29 +0900

    배포테스트

--- foori/src/scripts/before_install.sh
+++ foori/src/scripts/before_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# 기존 파일 정리
-if [ -d /home/ec2-user/Foori/foori/dist ]; then
-    sudo rm -rf /home/ec2-user/Foori/foori/dist/*
-fi
-
-# 디렉토리 생성 및 권한 설정
+# 기존 dist 폴더 정리
+sudo rm -rf /home/ec2-user/Foori/foori/dist
 sudo mkdir -p /home/ec2-user/Foori/foori/dist
 sudo chown -R ec2-user:ec2-user /home/ec2-user/Foori/foori/dist
-sudo chmod -R 755 /home/ec2-user/Foori/foori/dist
