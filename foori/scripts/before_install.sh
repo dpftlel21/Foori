da654636b2c7c32c23a9231730be24fbb78bdb27
dpftlel21 <dkdnmju@naver.com>
2025-01-23 10:06:57 +0900

    asdffdf

--- foori/scripts/before_install.sh
+++ foori/scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# 기존 배포 디렉토리 정리
-rm -rf /home/ec2-user/Foori/foori/dist
+# 기존 배포 디렉토리 정리 및 생성
+rm -rf /home/ec2-user/Foori
 mkdir -p /home/ec2-user/Foori/foori/dist
--- foori/scripts/start_application.sh
+++ foori/scripts/start_application.sh
@@ -3,5 +3,5 @@
 sudo chown -R ec2-user:ec2-user /home/ec2-user/Foori
 
 # nginx 설정 복사 및 재시작
-sudo cp /home/ec2-user/Foori/foori/nginx.conf /etc/nginx/conf.d/default.conf
-sudo systemctl restart nginx
+sudo cp -f /home/ec2-user/Foori/foori/nginx.conf /etc/nginx/conf.d/default.conf
+sudo systemctl restart nginx || echo "Failed to restart nginx"
