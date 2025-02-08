b44d876e269bb4f6082241dbf47f770b8dc08b8c
dpftlel21 <dkdnmju@naver.com>
2025-02-08 23:07:10 +0900

    fix: dvh 추가

--- foori/src/components/mypage/Content.tsx
+++ foori/src/components/mypage/Content.tsx
@@ -3,7 +3,7 @@ import Profile from './profile/Profile';
 
 const Content = () => {
   return (
-    <div className="w-full h-[calc(100dvh-5rem)] md:h-[calc(100dvh-20rem)] lg:h-[calc(100dvh-8rem)] md:max-w-[calc(100dvw-10rem)] flex flex-col bg-white md:border-solid md:border-2 md:border-[#EE6677] md:rounded-xl">
+    <div className="w-full h-[100dvh] md:h-[calc(100dvh-20rem)] lg:h-[calc(100dvh-8rem)] md:max-w-[calc(100dvw-10rem)] flex flex-col bg-white md:border-solid md:border-2 md:border-[#EE6677] md:rounded-xl">
       <Profile />
       <ContentList />
     </div>
