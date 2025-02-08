9a5d46622213e0194d6f3d9b87fe4c687e755114
dpftlel21 <dkdnmju@naver.com>
2025-02-08 22:58:17 +0900

    fix: viewport 설정 추가

--- foori/src/components/header/Header.tsx
+++ foori/src/components/header/Header.tsx
@@ -4,7 +4,9 @@ import Navigation from './Navigation';
 // 헤더 스타일
 const STYLES = {
   header: `
-    w-full h-20 md:h-20
+    w-full
+    h-[70px]
+    lg:h-[80px]
     transition-all duration-300
     fixed bottom-0 md:relative
     bg-gradient-to-r from-[#FFD4BC] to-[#FFBEA3]
